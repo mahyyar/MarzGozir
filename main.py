510b417f3c4ad29cd4b383a866e01b386f0bd472
MahYaR <mohammadetayy@gmail.com>
2025-05-30 11:42:22 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
+from aiogram.client.session.aiohttp import AiohttpSession
+from aiogram.client.bot import DefaultBotProperties
 from bot.handlers import start, button_callback, message_handler
 from bot_config import TOKEN
 
@@ -8,20 +10,21 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 async def main():
-    bot = Bot(token=TOKEN)
-    dp = Dispatcher()
+    session = AiohttpSession(timeout=30)
+    bot = Bot(token=TOKEN, session=session, default=DefaultBotProperties(parse_mode="HTML"))
+    dp = Dispatcher(max_retries=5, retry_delay=2)
     
     dp.message.register(start, Command(commands=["start"]))
     dp.callback_query.register(button_callback)
     dp.message.register(message_handler)
     try:
         logger.info("Starting bot...")
-        await dp.start_polling(bot)
+        await dp.start_polling(bot, polling_timeout=20)
     finally:
         await bot.session.close()
 
 if __name__ == "__main__":
     from database.db import init_db
-    init_db()  # Ensure DB is created if not exists
+    init_db()
     import asyncio
     asyncio.run(main())
